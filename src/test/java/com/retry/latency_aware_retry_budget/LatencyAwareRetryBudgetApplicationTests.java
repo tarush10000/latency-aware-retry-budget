0eb9a0a88405ba1a96a03849138a0a35adb9c625
Tarush Agarwal <tarushagarwal2003@gmail.com>
2025-11-06 01:30:57 +0530

    first commit

---
+++ src/test/java/com/retry/latency_aware_retry_budget/LatencyAwareRetryBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retry.latency_aware_retry_budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatencyAwareRetryBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
